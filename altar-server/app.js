d3d0827b3a5a812322410af5411c37995fd156b3
Carlos Gonçalves <cecg31@hotmail.com>
2024-03-02 01:08:29 +0000

    Added bias algorithm function

--- altar-server/app.js
+++ altar-server/app.js
@@ -1,38 +1,41 @@
-const WebSocket = require('ws');
-const http = require('http');
+const WebSocket = require("ws");
+const http = require("http");
 
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const server = http.createServer();
 const websocketServer = new WebSocket.Server({ noServer: true });
 const connections = new Map();
+const biasAmount = 20;
 
-websocketServer.on('connection', (websocket) => {
-  console.log('New connection established');
+websocketServer.on("connection", (websocket) => {
+  console.log("New connection established");
   connections.set(websocket, {
     grid: null,
-    code: null
+    code: null,
   });
 
-  websocket.on('message', (message) => {
+  websocket.on("message", (message) => {
     const gridAndChars = generateRandomChars();
-    const code = getLiveCodeAlgorithm(gridAndChars.grid, gridAndChars.charsCount);
+    const code = getLiveCodeAlgorithm(
+      gridAndChars.grid,
+      gridAndChars.charsCount
+    );
     connections.get(websocket).grid = gridAndChars.grid;
     connections.get(websocket).code = code;
     console.log(connections.get(websocket));
-    websocket.send(JSON.stringify(connections.get(websocket)))
+    websocket.send(JSON.stringify(connections.get(websocket)));
   });
 
-  websocket.on('close', ()=> {
-    console.log('Connection closed');
+  websocket.on("close", () => {
+    console.log("Connection closed");
     connections.delete(websocket);
     console.log(connections);
   });
-
 });
 
-server.on('upgrade', (request, socket, head) => {
+server.on("upgrade", (request, socket, head) => {
   websocketServer.handleUpgrade(request, socket, head, (websocket) => {
-    websocketServer.emit('connection', websocket, request);
+    websocketServer.emit("connection", websocket, request);
   });
 });
 
@@ -56,16 +59,36 @@ function generateRandomChars() {
   for (let index = 0; index < 10; index++) {
     const rowOfAlpha = [];
     for (let index = 0; index < 10; index++) {
-      const generatedChar = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+      const generatedChar = alphabet.charAt(
+        Math.floor(Math.random() * alphabet.length)
+      );
       /* If letter hasn't been added before, add an entry to the record */
-      numOfCharsGenerated[generatedChar] = numOfCharsGenerated[generatedChar] || 0;
+      numOfCharsGenerated[generatedChar] =
+        numOfCharsGenerated[generatedChar] || 0;
       numOfCharsGenerated[generatedChar]++;
       rowOfAlpha.push(generatedChar);
     }
     grid.push(rowOfAlpha);
   }
-
+  
   return { grid: grid, charsCount: numOfCharsGenerated };
+}
+
+function applyBiasToGrid(biasChar, grid, totalCharsInGrid) {
+  /* Takes into consideration that the grid might already have some x amount of the bias character
+    subtracting the 20 (20% of 100) by the current number of bias chars in the grid */
+  let totalOfBiasChars = totalCharsInGrid[biasChar.toUpperCase()];
+
+  while(totalOfBiasChars < biasAmount) {
+    const randomX = Math.floor(Math.random() * 10);
+    const randomY = Math.floor(Math.random() * 10);
+
+    /* Prevents the algorithm from replacing an already matching coordinate */
+    if(grid[randomX][randomY].toUpperCase() != biasChar.toUpperCase()) {
+      grid[randomX][randomY] = biasChar;
+      totalOfBiasChars++;
+    }
+  }
 }
 
 function getLiveCodeAlgorithm(grid, numOfCharsGenerated) {
@@ -85,5 +108,5 @@ function getLiveCodeAlgorithm(grid, numOfCharsGenerated) {
   const firstDigit = numOfCharsGenerated[firstLetter];
   const secondtDigit = numOfCharsGenerated[secondLetter];
 
-  return '' + firstDigit + secondtDigit;
+  return "" + firstDigit + secondtDigit;
 }
