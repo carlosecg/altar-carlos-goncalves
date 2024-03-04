13c795bc63cefdade6515b236db79df957c0a959
Carlos Gonçalves <cecg31@hotmail.com>
2024-03-04 17:24:20 +0000

    Added test to grid component

--- altar-app/src/app/app.component.spec.ts
+++ altar-app/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { GridGeneratorComponent } from '../components/grid-generator/grid-generator.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +10,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        GridGeneratorComponent
       ],
     }).compileComponents();
   });
@@ -20,16 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'altar-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('altar-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, altar-app');
-  });
 });
--- altar-app/src/components/grid-generator/grid-generator.component.spec.ts
+++ altar-app/src/components/grid-generator/grid-generator.component.spec.ts
@@ -1,23 +1,82 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GridGeneratorComponent } from './grid-generator.component';
+import { GridService } from '../../services/grid.service';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import { GridData } from '../../models/grid.model';
 
 describe('GridGeneratorComponent', () => {
   let component: GridGeneratorComponent;
   let fixture: ComponentFixture<GridGeneratorComponent>;
+  let gridServiceSpy: jasmine.SpyObj<GridService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [GridGeneratorComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    const gridServiceSpyObj = jasmine.createSpyObj('GridService', [
+      'changeBiasInput',
+      'stopUpdate',
+      'generateGrid',
+    ]);
+
+    TestBed.configureTestingModule({
+      declarations: [GridGeneratorComponent],
+      imports: [FormsModule],
+      providers: [{ provide: GridService, useValue: gridServiceSpyObj }],
+    });
+
     fixture = TestBed.createComponent(GridGeneratorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    gridServiceSpy = TestBed.inject(GridService) as jasmine.SpyObj<GridService>;
+
+    gridServiceSpy.gridDataSubject = new BehaviorSubject<GridData>({
+      grid: [[]],
+      code: 25,
+    });
+    gridServiceSpy.liveStatus = new BehaviorSubject(false);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should bind ngModel to biasChar input', async () => {
+    fixture.detectChanges();
+    const inputElement = fixture.debugElement.query(
+      By.css('input')
+    ).nativeElement;
+
+    component.biasChar = 'A';
+    inputElement.value = 'A';
+    inputElement.dispatchEvent(new Event('input'));
+
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    expect(component.biasChar).toBe('A');
+  });
+
+  it('should disable the input when allowBiasChange is false', async () => {
+    component.allowBiasChange = false;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const inputElement = fixture.debugElement.query(
+      By.css('#biasValue')
+    ).nativeElement;
+    expect(inputElement.disabled).toBe(true);
+  });
+
+  it('should disable the "Generate 2D Grid" button when allowBiasChange or validBiasCharacter is false', () => {
+    component.allowBiasChange = false;
+    fixture.detectChanges();
+    const generateButton = fixture.debugElement.query(
+      By.css('button')
+    ).nativeElement;
+    expect(generateButton.disabled).toBe(true);
+
+    component.allowBiasChange = true;
+    component.validBiasCharacter = false;
+    fixture.detectChanges();
+    expect(generateButton.disabled).toBe(true);
+
+    component.allowBiasChange = true;
+    component.validBiasCharacter = true;
+    fixture.detectChanges();
+    expect(generateButton.disabled).toBe(false);
   });
 });
