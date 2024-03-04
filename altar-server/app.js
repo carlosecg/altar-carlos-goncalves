60ef120d350326078094e633e414be1bec5e1d1c
Carlos Gonçalves <cecg31@hotmail.com>
2024-03-04 15:26:24 +0000

    Removed more unwanted code

--- altar-server/app.js
+++ altar-server/app.js
@@ -4,15 +4,5 @@ const WebSocketService = require('./controllers/websocket.service');
 const webSocketService = new WebSocketService();
 
 const server = http.createServer();
-
 webSocketService.createSocketServer();
 
-// server.on("upgrade", (request, socket, head) => {
-//   websocketServer.handleUpgrade(request, socket, head, (websocket) => {
-//     websocketServer.emit("connection", websocket, request);
-//   });
-// });
-
-// server.listen(WEBSOCKET_PORT, () => {
-//   console.log(`Websocket server is listening on port ${WEBSOCKET_PORT}`);
-// });
